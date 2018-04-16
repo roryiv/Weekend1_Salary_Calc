ab0e35bf95e74f96d1ff2261ff60f31bee7be38b
Rory Ivance <roryvance@gmail.com>
2018-04-15 22:56:37 -0500

    completed stretch mode

--- client.js
+++ client.js
@@ -1,24 +1,45 @@
 let monthlyCosts = 0;
 let employees = [];
+let newGuy;
 
 class Employee {
   constructor(name, id, title, salary) {
     this.name = name;
     this.id = id;
     this.title = title;
-    this. salary = salary;
+    this.salary = salary;
   }
 }
 
 $(document).ready(function(){
   $('#submitButton').on('click', function() {
-    addEmp();
-    updateCosts();
-    clearFields();
+    let validID = true;
+    for (let employee of employees) {
+      if ($(idIn).val() == employee.id) {
+        window.alert('ID Number in use. Please try again.');
+        validID = false;
+      }
+    }
+    if (validID) {
+      addEmp();
+      updateCosts(Number($(salaryIn).val())/12);
+      clearFields();
+    }
   });
   $('#employeeTable').on('click', '.deleteButton', function() {
-    $(this).parent().parent().remove();
-    console.log("Employee Deleted");
+    newGuy = false;
+    let currentRow = $(this).parent().parent();
+    let id = currentRow.find('td').eq(2).text();
+    let salaryToRemove;
+    for (i = 0; i < employees.length; i++) {
+      if (employees[i].id == id) {
+        salaryToRemove = Number(employees[i].salary)/12;
+        employees.splice(i,1);
+        break;
+      }
+    }
+    updateCosts(salaryToRemove);
+    currentRow.remove();
   });
 });
 
@@ -27,26 +48,49 @@ function addEmp() {
   let lastName = $(lastNameIn).val();
   let id = $(idIn).val();
   let title = $(titleIn).val();
-  let sal = $(salaryIn).val();
+  // let sal = $(salaryIn).val();
+  sal = parseFloat($(salaryIn).val()).toFixed(2);
   $('#empData').append('<tr class="anEmployee"><td>'+firstName+
   '</td><td>'+lastName+'</td><td>'+
-  id+'</td><td>'+title+'</td><td>$'+sal+
-  '.00</td><td><button class="deleteButton">Delete Employee</button></td></tr>');
+  id+'</td><td>'+title+'</td><td>$'+formatDollars(sal)+
+  '</td><td><button class="deleteButton">Delete Employee</button></td></tr>');
   let name = firstName+' '+lastName;
   let emp = new Employee(name, id, title, sal);
   employees.push(emp);
-  sal = Number(sal)/12;
-  monthlyCosts = parseFloat(sal+Number(monthlyCosts)).toFixed(2);
+  newGuy = true;
 }
 
-function updateCosts() {
+function updateCosts(amount) {
+    if (newGuy) {
+      let newCost = amount;
+      monthlyCosts = parseFloat(newCost+Number(monthlyCosts)).toFixed(2);
+    }
+    else {
+      let oldCost = amount;
+      monthlyCosts = parseFloat(Number(monthlyCosts)-oldCost).toFixed(2);
+    }
+  $('#monthlyTot').empty();
   if (monthlyCosts > 20000) {
-    $('#monthlyTot').empty();
-    $('#monthlyTot').append('<p style="background-color: red;">Total Monthly Cost: $'+monthlyCosts+'</p>');
+    $('#monthlyTot').append('<p style="background-color: red;">Total Monthly Cost: $'+
+    formatDollars(monthlyCosts)+'</p>');
   }
   else {
-    $('#monthlyTot').empty();
-    $('#monthlyTot').append('<p>Total Monthly Cost: $'+monthlyCosts+'</p>');
+    $('#monthlyTot').append('<p>Total Monthly Cost: $'+formatDollars(monthlyCosts)+'</p>');
+  }
+}
+
+function formatDollars(amount) {
+  amount = amount.toString();
+  let toFormat = amount.split('.');
+  if (toFormat[0].length >= 4) {
+    dollars = toFormat[0].split('');
+    dollars.splice(-3,0,',');
+    toFormat[0] = dollars.join('');
+    let formatted = toFormat.join('.');
+    return formatted;
+  }
+  else {
+    return amount;
   }
 }
 
